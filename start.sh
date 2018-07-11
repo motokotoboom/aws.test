906aafaa8906940c3dc1bd2d6fbdbbdb16d40b95
Moto Koto <motokotoboom@gmail.com>
2018-07-11 16:33:34 +0700

    some fixes

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-while [ ! -f /tmp/date.txt ]
-do
-  sleep 2
-  echo "waiting for dependencies installed"
-done
-sudo screen ./mnt/aws.test/deploy -chttp
+sudo /usr/bin/pip3 install boto3 httpserver paramiko psutil
+# while [ ! -f /tmp/date.txt ]
+# do
+#   sleep 2
+#   echo "waiting for dependencies installed"
+# done
+cd /mnt/aws.test
+sudo screen /mnt/aws.test/deploy -chttp

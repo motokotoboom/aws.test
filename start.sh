00bb904e4a95784be0a817a8b0e0e75b9a227617
Moto Koto <motokotoboom@gmail.com>
2018-07-12 21:31:06 +0700

    git commit comparison fix

--- start.sh
+++ start.sh
@@ -6,14 +6,14 @@ do
   echo "."
 done
 
-nohup /mnt/aws.test/deploy.py -chttp&
+sudo nohup ./deploy.py -chttp&
 cd /mnt/aws.test
 CURR=`git rev-parse HEAD`
 sudo git pull
 NEXT=`git rev-parse HEAD`
 echo $CURR $NEXT
-if [ "$CURR"!="$NEXT" ]; then
+if [ "$CURR" != "$NEXT" ]; then
   echo "restarting http service"
   sudo killall -9 deploy.py
-  sudo nohup /mnt/aws.test/deploy.py -chttp&
+  sudo nohup ./deploy.py -chttp&
 fi
